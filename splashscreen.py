75855a79719ffaf8e001fd1facc4616754080933
scottabramson <167094829+scottabramson@users.noreply.github.com>
2024-04-15 09:41:49 -0400

    Update splashscreen.py

--- splashscreen.py
+++ splashscreen.py
@@ -1,1 +1,39 @@
+import pygame
+WIDTH = 1280
+HEIGHT = 720
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Menu")
+BGCOLOR = (0, 0, 0)
+WHITE = (255, 255, 255)
+TITLE = "Made By Scott and Cooper"
 
+class splashscreen:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+        # Any other initializations here
+
+    def draw_text(self, text, size, color, x, y):
+        font = pygame.font.Font(None, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYUP:
+                    waiting = False
+    def show_start_screen(self): # game splash/start screen
+        self.screen.fill(BGCOLOR)
+        self.draw_text(TITLE, 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("We hope you enjoy.", 22, WHITE, WIDTH / 2, HEIGHT / 2)
+        self.draw_text("Press any key to play", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        pygame.display.flip()
+        self.wait_for_key()
