a2d55783969660a31bc945eda0910e67f26b6586
scottabramson <167094829+scottabramson@users.noreply.github.com>
2024-04-15 19:39:16 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,2 +1,84 @@
-import pygame
+import pygame as pg
+import sys
+from os import path
+from settings import *
+from sprites import *
+from tilemap import *
 
+
+
+class game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.load_data()
+
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        img_folder = path.join(game_folder, 'img')
+        map_folder = path.join(game_folder, 'maps')
+        self.map = TiledMap(path.join(map_folder, 'level1.tmx'))
+        self.map_img = self.map.make_map()
+        self.map_rect = self.map_img.get_rect()
+        self.player_img = pg.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
+
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.player = Player(self, 5, 5)
+        self.camera = Camera(self.map.width, self.map.height)
+
+    def run(self):
+        # game loop - set self.playing = False to end the game
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000.0
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
+        self.camera.update(self.player)
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        pg.display.set_caption("{:.2f}".format(self.clock.get_fps()))
+        # Clear the screen every frame
+        self.screen.fill(BGCOLOR)
+        # Draw the map
+        self.screen.blit(self.map_img, self.camera.apply_rect(self.map_rect))
+        # Draw all sprites in the all_sprites group
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
+        # If you want to draw the grid over the map and sprites, uncomment the following line
+        # self.draw_grid()
+        # After everything else, flip the display to show the new frame
+        pg.display.flip()
+
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
