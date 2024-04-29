4d99cc149c28bb04d8b0bc45fc57508afce3ed00
scottabramson <167094829+scottabramson@users.noreply.github.com>
2024-04-29 09:04:24 -0400

    Update game.py

--- game.py
+++ game.py
@@ -46,8 +46,8 @@ class Game:
         self.player_img = pg.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
         self.bullet_img = pg.image.load(path.join(img_folder, BULLET_IMG)).convert_alpha()
         self.mob_img = pg.image.load(path.join(img_folder, MOB_IMG)).convert_alpha()
-        self.wall_img = pg.image.load(path.join(img_folder, WALL_IMG)).convert_alpha()
-        self.wall_img = pg.transform.scale(self.wall_img, (TILESIZE, TILESIZE))
+        #self.wall_img = pg.image.load(path.join(img_folder, WALL_IMG)).convert_alpha()
+        #self.wall_img = pg.transform.scale(self.wall_img, (TILESIZE, TILESIZE))
 
     def new(self):
         # initialize all variables and do all the setup for a new game
